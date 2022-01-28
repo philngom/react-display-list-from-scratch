91877724f78f1e04c1fe66a210d331b413995d47
Philippe Ngom <philngom@gmail.com>
2022-01-27 19:47:18 -0800

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/This persons name/i);
+  const linkElement = screen.getByText(/name/i);
   expect(linkElement).toBeInTheDocument();
 });
